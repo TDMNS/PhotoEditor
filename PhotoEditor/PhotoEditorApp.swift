3dce8d0c2b5fdc42fb5964a6622c9557fee280aa
TDMNS <773o@mail.ru>
2022-10-12 15:57:45 +0700

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Oleg Kolbasa on 12.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
