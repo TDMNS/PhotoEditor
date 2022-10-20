0cac7332a0487b7b618d86996127d63122fb9e72
Oleg Kolbasa <773o@mail.ru>
2022-10-21 01:33:06 +0700

    Refactoring initial screen

--- PhotoEditor/AllowAccess.swift
+++ PhotoEditor/AllowAccess.swift
@@ -1,23 +1,33 @@
 import SwiftUI
 import Lottie
 
-@available(iOS 13.0, *)
 struct AllowAccess: View {
+    
+    private let lottieName = "duck"
+    private let textTitle = "Access Your Photos and Videos"
+    private let lottieWidth: CGFloat = 144
+    private let lottieHeight: CGFloat = 144
+    private let paddingLottieAndText: CGFloat = 20
+    private let fontSize: CGFloat = 20
+    private let paddingTextAndShimmer: CGFloat = 28
+    
     var body: some View {
         ZStack {
             Color.black.ignoresSafeArea()
             VStack {
-                LottieView(name: "duck", loopMode: .loop)
-                    .frame(width: 144, height: 144)
-                    .padding(.bottom, 20)
-                Text("Access Your Photos and Videos")
-                    .font(.system(size: 20))
+                LottieView(name: lottieName, loopMode: .loop)
+                    .frame(width: lottieWidth, height: lottieHeight)
+                    .padding(.bottom, paddingLottieAndText)
+                Text(textTitle)
+                    .font(.system(size: fontSize))
                     .fontWeight(.semibold)
                     .foregroundColor(.white)
                     .multilineTextAlignment(.center)
-                    .lineLimit(1)
-                    .padding(.bottom, 28)
-                ShimmerButton()
+                    .lineLimit(nil)
+                    .padding(.bottom, paddingTextAndShimmer)
+                ShimmerButton {
+                    print("Action on click button")
+                }
             }
         }
     }
@@ -32,41 +42,54 @@ struct ContentView_Previews: PreviewProvider {
 struct ShimmerButton: View {
     @State var isShimmerShowed = false
     var shimmerArea = (UIScreen.main.bounds.width / 2) + 110
+    let action: () -> Void
+    
+    private let fontSize: CGFloat = 17
+    private let viewHeight: CGFloat = 50
+    private let minWidth: CGFloat = 0
+    private let maxWidth: CGFloat = .infinity
+    private let viewCornerRadius: CGFloat = 10
+    private let padding: CGFloat = 16
+    private let colorOpacity: CGFloat = 0.3
+    private let gradientOpacity: CGFloat = 0.8
+    private let rectangleWidth: CGFloat = 200
+    private let animationSpeed: CGFloat = 0.3
+    private let animationDelay: CGFloat = 0
     
     var body: some View {
         ZStack {
             Button {
-                print("Action on click button")
+                action()
             } label: {
                 Text("Allow Access")
-                    .font(.system(size: 17))
+                    .font(.system(size: fontSize))
                     .fontWeight(.semibold)
-                    .frame(height: 50)
-                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: viewHeight)
+                    .frame(minWidth: minWidth, maxWidth: maxWidth)
                     .foregroundColor(.white)
                     .background(.blue)
-                    .cornerRadius(10)
-                    .padding([.leading, .trailing], 16)
+                    .cornerRadius(viewCornerRadius)
+                    .padding([.leading, .trailing], padding)
             }.contentShape(Rectangle())
             
-            Color.white.opacity(0.3)
-                .frame(height: 50)
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .cornerRadius(10)
-                .padding([.leading, .trailing], 16)
+            Color.white.opacity(colorOpacity)
+                .frame(height: viewHeight)
+                .frame(minWidth: minWidth, maxWidth: maxWidth)
+                .cornerRadius(viewCornerRadius)
+                .padding([.leading, .trailing], padding)
                 .allowsHitTesting(false)
             .mask(
                 Rectangle()
                     .fill(
-                        LinearGradient(gradient: .init(colors: [.clear, Color.white.opacity(0.8), .clear]), startPoint: .leading, endPoint: .trailing)
+                        LinearGradient(gradient: .init(colors: [.clear, Color.white.opacity(gradientOpacity), .clear]), startPoint: .leading, endPoint: .trailing)
                     )
-                    .frame(width: 200)
-                    .cornerRadius(10)
+                    .frame(width: rectangleWidth)
+                    .cornerRadius(viewCornerRadius)
                     .offset(x: isShimmerShowed ? shimmerArea : -shimmerArea)
             )
         }
         .onAppear {
-            withAnimation(Animation.default.speed(0.3).delay(0)
+            withAnimation(Animation.default.speed(animationSpeed).delay(animationDelay)
                 .repeatForever(autoreverses: false)) {
                     isShimmerShowed.toggle()
                 }
