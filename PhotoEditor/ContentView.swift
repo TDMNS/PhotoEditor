d987a848fe714409d81b9b9691317ba8964eea2b
MihailLos <enderclub12@gmail.com>
2022-10-20 15:53:07 +0700

    Added button shimmer effect

--- PhotoEditor/ContentView.swift
+++ PhotoEditor/ContentView.swift
@@ -1,6 +1,7 @@
 import SwiftUI
 import Lottie
 
+@available(iOS 13.0, *)
 struct ContentView: View {
     var body: some View {
         ZStack {
@@ -16,19 +17,7 @@ struct ContentView: View {
                     .multilineTextAlignment(.center)
                     .lineLimit(1)
                     .padding(.bottom, 28)
-                Button {
-                    print("Action on click button")
-                } label: {
-                    Text("Allow Access")
-                        .font(.system(size: 17))
-                        .fontWeight(.semibold)
-                        .frame(height: 50)
-                        .frame(minWidth: 0, maxWidth: .infinity)
-                        .foregroundColor(.white)
-                        .background(.blue)
-                        .cornerRadius(10)
-                        .padding([.leading, .trailing], 16)
-                }
+                ShimmerButton()
             }
         }
     }
@@ -37,5 +26,50 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ShimmerButton: View {
+    @State var isShimmerShowed = false
+    var shimmerArea = (UIScreen.main.bounds.width / 2) + 110
+    
+    var body: some View {
+        ZStack {
+            Button {
+                print("Action on click button")
+            } label: {
+                Text("Allow Access")
+                    .font(.system(size: 17))
+                    .fontWeight(.semibold)
+                    .frame(height: 50)
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .foregroundColor(.white)
+                    .background(.blue)
+                    .cornerRadius(10)
+                    .padding([.leading, .trailing], 16)
+            }.contentShape(Rectangle())
+            
+            Color.white.opacity(0.3)
+                .frame(height: 50)
+                .frame(minWidth: 0, maxWidth: .infinity)
+                .cornerRadius(10)
+                .padding([.leading, .trailing], 16)
+                .allowsHitTesting(false)
+            .mask(
+                Rectangle()
+                    .fill(
+                        LinearGradient(gradient: .init(colors: [.clear, Color.white.opacity(0.8), .clear]), startPoint: .leading, endPoint: .trailing)
+                    )
+                    .frame(width: 200)
+                    .cornerRadius(10)
+                    .offset(x: isShimmerShowed ? shimmerArea : -shimmerArea)
+            )
+        }
+        .onAppear {
+            withAnimation(Animation.default.speed(0.3).delay(0)
+                .repeatForever(autoreverses: false)) {
+                    isShimmerShowed.toggle()
+                }
+        }
     }
 }
